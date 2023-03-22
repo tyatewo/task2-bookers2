f51420313ede7e59e9b92a6e26c168ff6d73f9f4
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-03-22 09:13:17 +0000

    task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,19 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def new
+    @book = Book.new
+  end
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
+
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -15,7 +24,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -28,19 +37,26 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
+
+  def is_matching_login_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to (books_path) unless @user == current_user
+  end
+
 end
