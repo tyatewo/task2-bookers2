6b2c50d316609026a7ac3176a181b368983fa383
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-04-02 21:05:35 +0000

    task6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
+    render replace_btn
     #redirect_to request.referer
   end
 
@@ -11,6 +12,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
+    render replace_btn
     #redirect_to request.referer
   end
 
